723526020a6da6bd1e33aa4884fa85ef223f32c3
sillyangel <sillyangel3@gmail.com>
2022-12-15 18:46:19 +0000

    fart

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function playbuttonclicked() {
     const sb = document.querySelector('#chose');
-    if(client == sb.selectedIndex) {
+    if(1 == sb.selectedIndex) {
         window.location.href('native/index.html');
     } else {
         window.location.href('pc/index.html');
